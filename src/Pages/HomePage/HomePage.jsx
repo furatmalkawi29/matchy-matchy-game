b9e22ba38547578c666893b6c24b24f9c923a405
Furat <furatmalkawi9@gmail.com>
2023-07-30 14:23:32 +0300

    set up home page

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './HomePage.module.scss'
+export const HomePage =()=>{
+
+    return (
+        <div className={styles.test}>
+            <h1>HomePage</h1>
+        </div>
+    )
+}
